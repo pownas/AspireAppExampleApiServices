6c6873d93e5f4f2fffae58b63dbed8344b5ceb2d
pownas <pownas@outlook.com>
2026-06-30 22:22:00 +0200

    Adding 2 more ApiServices (2 and 3)

--- AspireApp1.AppHost/AppHost.cs
+++ AspireApp1.AppHost/AppHost.cs
@@ -1,7 +1,17 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
+
+var apiService2 = builder.AddProject<Projects.AspireApp1_ApiService2>("apiservice2")
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+var apiService3 = builder.AddProject<Projects.AspireApp1_ApiService3>("apiservice3")
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService2)
+    .WaitFor(apiService2);
 
 builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
